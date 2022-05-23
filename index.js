ec5a3a4a06a5926964be698f74a9d81c6a9a5d58
JITESH KUMAR SHIVGAN <jiteshshivgan5@gmail.com>
2022-05-23 13:36:05 +0530

    Setup the database

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app=express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+app.use(express.urlencoded());
+
+app.use(express.static('assets'));
+
+
+
 app.get('/', function(request, response){
     response.render('home');
 })
